90ce96f0103ee683ad14315fbc093452c95c9b31
Edwin Kato <katedwin88@gmail.com>
2017-04-20 18:04:20 +0300

    Modify binary search to pass all tests

--- binary_search.py
+++ binary_search.py
@@ -4,21 +4,25 @@ class BinarySearch(list):
         self.length = len(self.array)
         super(BinarySearch, self).__init__(self.array)
 
-    def search(self, param):
+    def search(self, value_to_search):
         count = 0
         array = self.array
         smaller = 0
         bigger = len(array)
+
+        if value_to_search == array[-1] : return {"count": count, "index": bigger - 1}
+        if value_to_search == array[0]: return {"count": count, "index": smaller}
+
         while smaller < bigger:
-            count += 1
             middle = smaller + (bigger - smaller) // 2
             value = self.array[middle]
-            if param == value:
+            if value_to_search == value:
                 return {"count": count, "index": middle}
-            elif param > value:
+            elif value_to_search > value:
                 if smaller == middle:
                     break
                 smaller = middle
             else:
                 bigger = middle - 1
-        return {"count": count, "index": -1}
+            count += 1
+        return {"count": count-1, "index": -1}
